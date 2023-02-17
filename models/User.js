6e89e3f6fd45b0e98bf4c891a2ced52ee21b99f9
BeYoddy <changwoon4@naver.com>
2023-02-18 01:40:35 +0900

    토큰생성

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -13,7 +15,6 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    minlength: 5,
   },
   lastname: {
     type: String,
@@ -31,6 +32,7 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
 userSchema.pre("save", function (next) {
   var user = this;
   if (user.isModified("password")) {
@@ -42,9 +44,27 @@ userSchema.pre("save", function (next) {
         next();
       });
     });
+  } else {
+    next();
   }
 });
 
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  var token = jwt.sign(user._id, "secretToken");
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
